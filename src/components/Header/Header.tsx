a0052dcf2bf4ebf259e3d0afda90f64ea2d300b5
AbelTeha <abelteha@gmail.com>
2022-11-16 10:23:08 +0300

    finished designing the collapsable menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,10 +8,12 @@ import SearchForm from "./SearchForm";
 import { Fragment, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../model/hooks";
 import { searchAction } from "../../redux/search-slice";
+import { Catagories } from "../../model/catagories";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const [toggleOpen, setToggleOpen] = useState(false);
   const searchPressed = useAppSelector(
     (state) => state.searchReducer.searchButtonPressed
   );
@@ -20,9 +22,13 @@ const Header = () => {
     dispatch(searchAction.toggleSearchBar(!searchPressed));
   };
 
+  const hamburgerMenuHandler = () => {
+    setToggleOpen((prev) => !prev);
+  };
+
   return (
     <Fragment>
-      <div className="sticky top-0 animate-slidedown backdrop-blur-xl z-10">
+      <div className="sticky top-0 animate-slidedown backdrop-blur-xl z-20">
         <div className=" flex justify-between p-4  bg-white/70  items-center lg:px-[6rem]  py-5 border-b-2 ">
           <img
             src={logo}
@@ -49,17 +55,33 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <div className="flex border-b-2 py-1 px-5 bg-gray-300/50 justify-end ">
-          <Link to={"/"} className="text-sm hover:text-[#a75b29] pr-5">
-            All Categories
-          </Link>
-          <Link to={"/auth"} className="text-sm hover:text-[#a75b29]">
-            SignIn
-          </Link>
+        <div className="flex border-b-2 py-1 px-5 bg-gray-300/50 justify-between md:justify-end ">
+          <button
+            id="menu-btn"
+            className={`block ${
+              toggleOpen ? "open" : ""
+            } hamburger md:hidden focus:outline-none`}
+            onClick={hamburgerMenuHandler}
+          >
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </button>
+          <div className="flex gap-5">
+            <Link to={"/"} className="text-sm hover:text-[#a75b29] ">
+              All Categories
+            </Link>
+            <Link to={"/auth"} className="text-sm hover:text-[#a75b29]">
+              SignIn
+            </Link>
+            <Link to={"/"} className="text-sm hover:text-[#a75b29]">
+              Logout
+            </Link>
+          </div>
         </div>
       </div>
       {searchPressed && (
-        <div className="sticky z-10 bg-black/50 backdrop-blur-xl bg-opacity-60 top-[6.8rem] ">
+        <div className="sticky z-10 bg-black/50 backdrop-blur-xl bg-opacity-60 top-[6.9rem] ">
           <SearchForm value="pt-2" />
           <div
             className="absolute bg-black bg-opacity-60 w-full h-[calc(100vh-8.5rem)]"
@@ -69,7 +91,29 @@ const Header = () => {
           ></div>
         </div>
       )}
-      {}
+      <div className="sticky top-[6.9rem] z-10">
+        <div
+          className={`absolute top-[0rem] h-[calc(100vh-7rem)] w-2/3 bg-white/70 z-10 p-6  pl-[15%] md:hidden duration-300 backdrop-blur-xl overflow-scroll ${
+            toggleOpen ? "left-0" : "-left-full"
+          }`}
+        >
+          <h1 className="text-xl font-bold my-3">Catagories</h1>
+          <ul>
+            {Catagories.map((cat) => (
+              <Link
+                to={`/categories/${cat.title}`}
+                className="flex items-center text-sm sm:text-md mb-4 gap-2"
+                onClick={hamburgerMenuHandler}
+              >
+                {cat.icon}
+                <li className="" key={cat.title}>
+                  {cat.title}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
     </Fragment>
   );
 };
